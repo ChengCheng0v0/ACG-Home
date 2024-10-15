daf27d0bccff0ae6e039832918027c2d6b68d1d6
成成0v0 <chengcheng@miao.ms>
2024-10-16 05:56:08 +0800

    使网站标题和页首 Typed 打印内容受到网站配置文件控制，引入 Alpine.js
    此次提交还加入了好看的控制台输出！其中包含了欢迎语和项目的 GitHub 链接。

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,15 +1,31 @@
-/* global getWebsiteConfig, swal, renderMarkdown */
+/* global getWebsiteConfig, Typed, swal, renderMarkdown */
 
 document.addEventListener("DOMContentLoaded", async () => {
     // 获取网站配置
     const config = getWebsiteConfig();
     config.init();
 
+    // 设置网站标题
+    document.title = config.content.title;
+
     // 输出控制台欢迎消息
-    console.log("欢迎来到 " + config.content.title + "！你在看什么呢？！");
+    console.log("%c欢迎来到%c" + config.content.title + "！", "padding: 5px; border-radius: 6px 0 0 6px; background-color: #1e88a8; color: #ffffff;", "padding: 5px; border-radius: 0 6px 6px 0; background-color: #b5495b; color: #ffffff;");
+    console.log("%c打开开发者工具是想干嘛呢？应该是想扒代码吧！项目是开源的哦 (GPL-v3)，你喜欢的话拿去用就是了！%c" + "o(〃'▽'〃)o", "padding: 5px; border-radius: 6px 0 0 6px; background-color: #00896c; color: #ffffff;", "padding: 5px; border-radius: 0 6px 6px 0; background-color: #986db2; color: #ffffff;");
+    console.log("%cGitHub.com/%c" + "ChengCheng0v0/ACG-Home", "padding: 5px; border-radius: 6px 0 0 6px; background-color: #010101; color: #ffffff;", "padding: 5px; border-radius: 0 6px 6px 0; background-color: #ff9901; color: #ffffff;");
 
     // 渲染 Markdown 内容
     renderMarkdown();
+
+    // 加载页首打字标题
+    var typed = new Typed(".page-head > .title", {
+        strings: config.content.pageHead.typedContent,
+        startDelay: 300,
+        backDelay: 1000,
+        typeSpeed: 100,
+        backSpeed: 50,
+        showCursor: true,
+        loop: true,
+    });
 
     // 获取 Hitokoto 一言
     fetch("https://v1.hitokoto.cn")
