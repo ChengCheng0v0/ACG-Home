3b887e1b738b58ef261e40aa60cf6ac9cc58b238
成成0v0 <chengcheng@miao.ms>
2024-12-25 18:32:47 +0800

    实现左侧区域跟随窗口滚动的效果

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,3 @@
-/* global config, Alpine, getWebsiteConfig, Typed, swal, renderMarkdown */
 /* global config, throttle, debounce, Alpine, getWebsiteConfig, Typed, swal, renderMarkdown */
 
 // 初始化 Alpine
@@ -9,6 +8,8 @@ document.addEventListener("alpine:init", () => {
 document.addEventListener("DOMContentLoaded", async () => {
     // 获取 DOM 元素
     var element = {
+        pageHead: document.querySelector(".page-head"),
+        leftArea: document.querySelector(".primary-container > .left-area"),
         socialIcons: document.querySelector(".social-icons"),
         icpInfo: document.querySelector(".icp-info"),
         webmasterInfo: document.querySelector(".webmaster-info"),
@@ -67,6 +68,19 @@ document.addEventListener("DOMContentLoaded", async () => {
             hitokoto.innerText = data.hitokoto;
         })
         .catch(console.error);
+
+    // 自动悬浮左侧区域
+    const pageHeadHeight = element.pageHead.clientHeight;
+    const updateFloatPageHeadMargin = debounce(() => {
+        element.leftArea.style.marginTop = window.scrollY - pageHeadHeight + "px";
+    }, 60);
+    document.addEventListener("scroll", () => {
+        if (window.scrollY >= pageHeadHeight) {
+            updateFloatPageHeadMargin();
+        } else {
+            element.leftArea.style.marginTop = "unset";
+        }
+    });
 
     /* 生成页脚 ICP 备案信息 */
 
